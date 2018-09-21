3b78c6e3041038def7027cd971aedecd70561650
Liwa <liwa.lemjid@esprit.tn>
2018-09-21 22:59:19 +0100

    Finish layout

---
+++ app/src/main/java/com/dev/liwa/swoosh/Controller/FinishActivity.kt
@@ -0,0 +1,19 @@
+package com.dev.liwa.swoosh.Controller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.dev.liwa.swoosh.R
+import com.dev.liwa.swoosh.Utilities.EXTRA_LEAGUE
+import com.dev.liwa.swoosh.Utilities.EXTRA_SKILL
+import kotlinx.android.synthetic.main.activity_finish.*
+
+class FinishActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_finish)
+        val league = intent.getStringExtra(EXTRA_LEAGUE)
+        val skill = intent.getStringExtra(EXTRA_SKILL)
+        searchLeagueText.text = "Looking for $league $skill league near you..."
+    }
+}
--- app/src/main/java/com/dev/liwa/swoosh/Controller/WelcomeActivity.kt
+++ app/src/main/java/com/dev/liwa/swoosh/Controller/WelcomeActivity.kt
@@ -1,8 +1,8 @@
-package com.dev.liwa.swoosh
+package com.dev.liwa.swoosh.Controller
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.dev.liwa.swoosh.R
 import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : BaseActivity() {
